0cc5528d7cc9f6113549bae0dd4a5853e2f9492f
Jared Wright <jtw57@gmail.com>
2015-04-22 20:50:40 -0400

    added things

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,13 @@ _msg = ""
 exports.homeGET = function(req, res) {	
 	var loggedIn;
 
-	if (typeof req.session.token == "undefined") {
-		loggedIn = false;
-	} else {
-		loggedIn = true;
+	if (typeof req.session != "undefined") {
+		if (typeof req.session.token == "undefined") {
+			loggedIn = false;
+		} 
 	}
-
+	
+	if (loggedIn != false) loggedIn = true;
 	if (typeof _msg == "undefined") _msg = "";
 	res.render("home", {title: _title, subTitle: "home", type: req.session.type || undefined, firstName: req.session.firstName || undefined, msg: _msg, loggedIn: loggedIn}); delete _msg;
 }
