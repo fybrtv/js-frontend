99796d6dc7b65de488285d25db3e6d20d0dab94e
Jared Wright <jtw57@gmail.com>
2015-04-29 02:40:06 -0400

    Frontend server implementation of timeslote retrieval

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ _title = "fybr";
 _firstName = "";
 _type = "0";
 _msg = "";
-var request = require("request");
 
 exports.homeGET = function(req, res) {
+    var currentChannel = req.query.ch || "Comedy";
     var loggedIn = true;
+
     request({
         uri: "http://127.0.0.1:5000/channels",
         method: "GET"
@@ -19,20 +20,49 @@ exports.homeGET = function(req, res) {
             }
             if (typeof _msg == "undefined") _msg = "";
             console.log(body)
-            console.log('channels', body.document);
-            res.render("home", {
-                title: _title,
-                subTitle: "home",
-                type: req.session.type || undefined,
-                firstName: req.session.firstName || undefined,
-                msg: _msg,
-                channels: JSON.parse(body).document,
-                loggedIn: loggedIn
-            });
-            delete _msg;
+            console.log('channels', JSON.parse(body).document);
+
+            for (var i = 0; i<JSON.parse(body).document.length; i++) {
+                if (JSON.parse(body).document[i].channelName == currentChannel) {
+                    var currentChannelId = JSON.parse(body).document[i]._id;
+                    break;
+                }
+            }
+
+            request.get("http://localhost:5000/timelines/"+currentChannelId, function(err, response1, body1) {
+                if (err) {
+                    console.log(response1)
+                    err.status = response1.status;
+                    res.render("error", {error: err});
+                } else {
+                    var data = JSON.parse(body)
+                    var videoInfo = {
+                        ts_index: data.ts_index,
+                        tl_index: data.tl_index,
+                        fileId: data.fileId,
+                        start: data.start
+                    }
+                    res.render("home", {
+                        title: _title,
+                        subTitle: "home",
+                        type: req.session.type || undefined,
+                        firstName: req.session.firstName || undefined,
+                        msg: _msg || null,
+                        channels: JSON.parse(body).document,
+                        loggedIn: loggedIn,
+                        currentChannel: currentChannel,
+                        videoInfo: videoInfo
+                    }, function() {
+                        delete _msg;
+                    }); 
+
+                }   
+            })
+            
+           
         } else {
             _msg = "Server malfunction";
-            res.redirect('/')
+            res.render("error", {error: error});
         }
 
     });
