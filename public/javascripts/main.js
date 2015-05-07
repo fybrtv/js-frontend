61a28895ad3fe17abe7c0326d2dc0f9cdad3f3bc
Jared Wright <jtw57@gmail.com>
2015-05-07 00:27:37 -0400

    Home design changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,45 @@
 console.log("Main.js initialized...")
+
+function DropDown(el) {
+    this.dd = el;
+    this.initEvents();
+}
+DropDown.prototype = {
+    initEvents : function() {
+        var obj = this;
+
+        obj.dd.on('click', function(event){
+            $(this).toggleClass('active');
+            event.stopPropagation();
+        }); 
+    }
+}
+
+$.fn.topText = function() {
+   
+    return $(this)  .clone()
+            .children()
+            .remove()
+            .end()
+            .text();
+ 
+};
+
+$(function() {
+
+	var dd = new DropDown( $('#dd') );
+
+	$(document).click(function() {
+		// all dropdowns
+		$('.wrapper-dropdown-1').removeClass('active');
+	});
+    $(document).ready(function(){
+        currentChannel = window.location.search.replace("?","").split("&")[0].split("=")[1] ||  "Comedy";
+
+        var dd = $('#dd').get(0);
+        var ctopText = dd.childNodes[0];
+        ctopText.nodeValue = currentChannel;
+
+        $("#videoStream").height($(window).innerHeight()-parseInt($("#header").css("height")))
+    });
+});
